a822c6c34eeeb9d328978279e5bf843d5f1f9af2
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2015-05-11 21:19:10 -0400

    ArcherIRC

---
+++ scrollback.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>ArcherSys SBFeeder</title>
+<script>window.scrollback = {"room":"archersysos","titlebarColor":"#ff8040","form":"toast","minimize":true};(function(d,s,h,e){e=d.createElement(s);e.async=1;e.src=(location.protocol === "https:" ? "https:" : "http:") + "//scrollback.io/client.min.js";d.getElementsByTagName(s)[0].parentNode.appendChild(e);}(document,"script"));</script>
+</head>
+<body>
+</body>
+</html>
